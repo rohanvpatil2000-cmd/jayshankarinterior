c42da3bcbf1a97bc5aaf0d6939d0ac659b5e2f5e
rohanvpatil2000-cmd <rohanvpatil2000@gmail.com>
2025-12-27 23:21:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function openPopup() {
-  document.getElementById("popup").style.display = "block";
+  document.getElementById("popup").style.display = "flex";
 }
 
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
+
+// ✅ AUTO OPEN POPUP AFTER 5 SECONDS
+window.onload = function () {
+  setTimeout(function () {
+    openPopup();
+  }, 5000);
+};
