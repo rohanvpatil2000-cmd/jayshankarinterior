01d2284c211f8f2629b25bbf668bdd46763df5d0
rohanvpatil2000-cmd <rohanvpatil2000@gmail.com>
2026-05-26 05:38:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Website loaded');
+
+function setupSlider(id, images){
+ let i=0; const el=document.getElementById(id); if(!el||!images.length) return;
+ el.src=images[0];
+ setInterval(()=>{i=(i+1)%images.length; el.src=images[i];},3000);
+}
